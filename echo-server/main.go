2c77ade3932e164121cc8182b0107d1fa1130b42
aranhams <aranha.mss@gmail.com>
2021-03-02 23:40:11 -0300

    improving the buffer reader and writer

--- echo-server/main.go
+++ echo-server/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"io"
+	"bufio"
 	"log"
 	"net"
 )
@@ -9,25 +9,20 @@ import (
 func echo(conn net.Conn) {
 	defer conn.Close()
 
-	b := make([]byte, 512)
 	for {
-		size, err := conn.Read(b[0:])
-		if err == io.EOF {
-			log.Printf("Client disconnected")
-			break
-		}
-
+		reader := bufio.NewReader(conn)
+		s, err := reader.ReadString('\n')
 		if err != nil {
-			log.Println("Unexpected error")
-			break
+			log.Fatalln("Unable to read data")
 		}
-
-		log.Printf("Received %d bytes: %s\n", (size), string(b))
+		log.Printf("Read %d bytes: %s", len(s), s)
 
 		log.Println("Writing data")
-		if _, err := conn.Write(b[0:size]); err != nil {
+		writer := bufio.NewWriter(conn)
+		if _, err := writer.WriteString(s); err != nil {
 			log.Fatalln("Unable to write data")
 		}
+		writer.Flush()
 	}
 }
 
